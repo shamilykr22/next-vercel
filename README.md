6361ab36c263d78571a4360a2fadf1c8b7438efd
shamilykr <shamily@keyvalue.systems>
2025-03-11 14:31:04 +0530

    Update dashboard layout and navigation components

--- README.md
+++ README.md
@@ -3,3 +3,45 @@
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+
+
+## 
+1. npx create-next-app@latest
+2. cd nextjs-dashboard
+3. npm install
+4. npm run dev
+
+
+Routing files 
+*********************
+
+layout -> 	.js .jsx .tsx	Layout  -> RootLayout for /app folder root level layout
+page	->  .js .jsx .tsx	Page
+loading	-> .js .jsx .tsx	Loading UI
+not-found	-> .js .jsx .tsx	Not found UI
+error	-> .js .jsx .tsx	Error UI
+global-error	-> .js .jsx .tsx	Global error UI
+route	-> .js .ts	API endpoint
+template	.js .jsx .tsx	Re-rendered layout
+default
+
+Nested routes
+*********************
+
+folder	-> Route segment
+folder/folder	-> Nested route segment
+
+
+Dynamic routes
+*************************
+
+[folder]	-> Dynamic route segment
+[...folder]	-> Catch-all route segment
+[[...folder]]	Optional catch-all route segment
+
+Route Groups and private folders
+*****************************************
+
+(folder)	Group routes without affecting routing
+_folder	Opt folder and all child segments out of routing
+
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,12 @@
+'use client';
+import Link from 'next/link';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -16,20 +20,33 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
+// Dynamic component that renders the navigation links based on the pathname.
+// If <a> tag then it will , full pgae is refreshed
+// So uses the <Link> , It helps for code splitting 
+//To improve the navigation experience, Next.js automatically code splits your application by route segments. 
+// Next.js automatically prefetches the code for the linked route in the background.
+// By the time the user clicks the link, the code for the destination page will already be loaded in the background,
 export default function NavLinks() {
+  // Used to get the current path name, Since it is a react hook, use it inside client componet
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
