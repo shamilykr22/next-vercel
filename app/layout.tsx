0dda697d9945224773997fdb697c264da6e0eb84
shamilykr <shamily@keyvalue.systems>
2025-03-26 14:19:53 +0530

    Added PWA and some BE codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,23 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
 // Parent layout page, its in defined insided the /app folder
 // All the pages inside will have this default style
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  manifest: '/manifest.json',
+  icons: {
+    apple: '/icon-192x192.png',
+  },
+  themeColor: '#000000',
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -10,6 +25,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
